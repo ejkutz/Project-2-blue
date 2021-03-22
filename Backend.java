a3370543db39a11d045964d39fc142f06735aa02
ejkutz <79886893+ejkutz@users.noreply.github.com>
2021-03-22 16:25:46 -0500

    Add files via upload

--- Backend.java
+++ Backend.java
@@ -18,12 +18,16 @@ public class Backend extends RedBlackTree<Delivery>{
   java.util.Date date=new java.util.Date();
   RedBlackTree<Delivery> finalTree= new RedBlackTree<Delivery>();
   List<Delivery> dlist;
+  /*Constructor with file
+   */
   public Backend (File file) {
     List<Delivery> dlist =getDeliveryObjects(file);
     for (int x=0;dlist.size()<x;x++) {
       finalTree.insert(dlist.get(x));
   }
   }
+  /*Constructor with filepath
+  */
   public Backend (String filepath) {
     List<Delivery> dlist =getDeliveryObjects(filepath);
     for (int x=0;dlist.size()<x;x++) {
@@ -40,19 +44,28 @@ public class Backend extends RedBlackTree<Delivery>{
       finalTree.insert(newdel.get(x));
     }
 }
-  public void addDelivery(String item, String location, int feeDelivery ,String datestr) {
-    Date now=new  Date();
+/*new Delivery with imputs of item location, fee and date deliverd, calculates order placed time and 
+ * difference between date delivered and current time returns how many days till delivery
+@ param String Item String location, int fee, and String date in dd/mm/yyyy format
+@returns days till delivery
+*/
+  public int addDelivery(String item, String location, int feeDelivery ,String datestr) {
+      Date now=new  Date();
     Date delivery=new  Date();
     try {
       delivery = new SimpleDateFormat("dd/MM/yyyy").parse(datestr);
     } catch (ParseException e) {
       System.out.println("Delivery Addition Failed imporoper date format.");
-      return;
+      return -1;
     }
     int day = (int) ((delivery.getTime()-now.getTime())/ (86400000) % 365); 
     finalTree.insert(new Delivery(item, day, feeDelivery, location, delivery, null, now));
+    return day;
   }
-    
+  /*Gets delivery based on itemname
+   * @returns Delivery with ItemName
+   * @param Item name to search for
+   */
   public Delivery getDelivery(String itemName){
     Iterator<Delivery> treeNodeIterator = this.iterator();
     if (treeNodeIterator.hasNext()) {
@@ -67,7 +80,10 @@ public class Backend extends RedBlackTree<Delivery>{
   }
 return null;
   }
-  
+  /*Gets delivery based on date
+   * @returns List with all deliveries on that date
+   * @param string of date in dd/mm/yyyy format
+   */
   public List<Delivery> getDeliverydate(String datestr){
     Date delivery=new  Date();
     List <Delivery>dlist=new ArrayList<Delivery>();
@@ -93,6 +109,10 @@ return null;
     return dlist;
     
 }
+  /*Gets delivery based on date
+   * @returns List with all deliveries on that date
+   * @param date of delivery
+   */
   public Delivery getDeliveryOn(Date date){
     Iterator<Delivery> treeNodeIterator = this.iterator();
     if (treeNodeIterator.hasNext()) {
