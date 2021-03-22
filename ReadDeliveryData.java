145a1c7c761801867d24356c063b4961039b5b72
ejkutz <79886893+ejkutz@users.noreply.github.com>
2021-03-22 16:14:54 -0500

    Add files via upload

---
+++ ReadDeliveryData.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.List;
+
+public interface ReadDeliveryData {
+
+  public static List<Delivery> getDeliveryObjects(String filePath ){
+    return null;
+    
+  }
+  public static List<Delivery> getDeliveryObjects(File file ){
+    return null;
+    
+  }
+  
+}
